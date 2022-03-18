59e602664ddf1face3d11a45dd41b3ee648b27c5
pcismyname <pcismyname007@gmail.com>
2022-03-18 17:53:17 +0700

    assign5

--- as5/script.js
+++ as5/script.js
@@ -1,13 +1,21 @@
-$(document).ready(function(){  $('#gallery a').hover(function(){     var galleryHref = $(this).attr('href');     var galleryAlt = $(this).attr('title');   $('figure img').attr({ src: galleryHref, alt: galleryAlt });      $('figcaption').html(galleryAlt);   });});
+//$(document).ready(function(){  $('#gallery a').hover(function(){     var galleryHref = $(this).attr('href');     var galleryAlt = $(this).attr('title');   $('figure img').attr({ src: galleryHref, alt: galleryAlt });      $('figcaption').html(galleryAlt);   });});
+
+
 
 $(document).ready(function (){
        $("#gallery a").click(function () {
         var galleryHref = $(this).attr('href');     
-        var galleryAlt = $(this).attr('title');     
+        var galleryAlt = $(this).attr('title');
+        $('figure').css('display','none');     
         $('figure img').attr({ src: galleryHref, alt: galleryAlt }); 
-        $('figcaption').html("<b>" + galleryAlt + "</b>");     
+        $('figcaption').html("<b>" + galleryAlt + "</b>");
+        $('figure').fadeToggle(1000); 
         return false;  
        });
     });
+
+
+
+
 
 
